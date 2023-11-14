248ba1d9a92b0d941874c90a2cf1ca3a64da8400
filip <102216221+FilippYV@users.noreply.github.com>
2023-11-14 15:33:20 +0300

    start cnn

--- 8. Convolutional_neural_networks/Convolutional_neural_networks.py
+++ 8. Convolutional_neural_networks/Convolutional_neural_networks.py
@@ -1,143 +1,121 @@
 import numpy as np
 import pandas as pd
+import random
+from sklearn.preprocessing import OneHotEncoder
 
 
 def get_train_data():
     data_train = pd.read_csv('../Datasets/Fashion MNIST/fashion-mnist_train.csv')
     X_train = data_train.drop(['label'], axis=1).to_numpy()
-    X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))  # Обновленный формат
+    X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))
 
     y_train = data_train['label'].to_numpy()
-    # print('x_train\n', X_train[:5])
-    # print()
-    # print('y_train\n', y_train[:5])
-    return X_train, y_train
+
+    # One-hot encode the labels
+    encoder = OneHotEncoder(sparse=False)
+    y_train_onehot = encoder.fit_transform(y_train.reshape(-1, 1))
+
+    return X_train, y_train_onehot
 
 
 class CNN:
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, num_classes, count_convolutional_layer):
         self.input_size = input_size
         self.num_classes = num_classes
-        self.filters1 = np.random.randn(3, 3, 1, 32)
-        self.filters2 = np.random.randn(2, 2, 32, 64)  # Уменьшение размера фильтров второго сверточного слоя
-        self.weights_fc = np.random.randn(7 * 7 * 64, 100)
-        self.weights_output = np.random.randn(100, num_classes)
+        self.count_convolutional_layer = count_convolutional_layer
+        self.weight_count_convolutional = self.get_weight_count_convolutional()
+        self.flatten_weight = self.get_flatten_weight()
 
-    def convolve2d(self, input_data, filters):
-        _, height, width, depth = input_data.shape
-        filter_height, filter_width, input_depth, num_filters = filters.shape
+    def get_flatten_weight(self):
+        flatten_weight = []
+        for i in range(self.num_classes):
+            mask = []
+            for j in range(49):
+                mask.append(random.uniform(0, 1))
+            flatten_weight.append(mask)
+        return np.array(flatten_weight)
 
-        output_height = height - filter_height + 1
-        output_width = width - filter_width + 1
-
-        output_data = np.zeros((output_height, output_width, num_filters))
-
-        for i in range(output_height):
-            for j in range(output_width):
-                # Используйте расширение осей для корректного выполнения операции свертки
-                output_data[i, j, :] = np.sum(
-                    input_data[:, i:i + filter_height, j:j + filter_width, np.newaxis] * filters,
-                    axis=(1, 2, 3))
-
-        return output_data
-
-    def max_pooling(self, input_data, pool_size=(2, 2), padding='valid'):
-        height, width, num_filters = input_data.shape
-        pool_height, pool_width = pool_size
-
-        if padding == 'valid':
-            output_height = height // pool_height
-            output_width = width // pool_width
-        elif padding == 'same':
-            output_height = height
-            output_width = width
-        else:
-            raise ValueError("Invalid padding mode. Use 'valid' or 'same'.")
-
-        output_data = np.zeros((output_height, output_width, num_filters))
-
-        for i in range(output_height):
-            for j in range(output_width):
-                output_data[i, j, :] = np.max(
-                    input_data[i * pool_height:(i + 1) * pool_height, j * pool_width:(j + 1) * pool_width, :],
-                    axis=(0, 1))
-
-        return output_data
+    def get_weight_count_convolutional(self):
+        weight_count_convolutional = [[0.1, 0.1, 0.1] for
+                                      i in range(3)]
+        return weight_count_convolutional
 
     def flatten(self, input_data):
         return input_data.reshape(-1)
 
-    def fully_connected(self, input_data, weights):
-        return np.dot(input_data, weights)
+    def convolve2d(self, input_data):
+        height, width = input_data.shape
+        mask = np.zeros((height + 2, width + 2))
+        mask[1:height + 1, 1:height + 1] = input_data
+        output_data = []
+        for i in range(1, height + 1):
+            s_array = []
+            for j in range(1, width + 1):
+                summ = 0
+                for ii in range(-1, 2):
+                    for jj in range(-1, 2):
+                        summ += mask[i + ii][j + jj] * self.weight_count_convolutional[ii + 1][jj + 1]
+                s_array.append(summ)
+            output_data.append(s_array)
+        return output_data
 
-    def softmax(self, x):
-        exp_x = np.exp(x - np.max(x))
-        return exp_x / exp_x.sum(axis=0, keepdims=True)
+    def max_pooling(self, data):
+        leng = len(data)
+        i = 0
+        j = 0
+        result = []
+        while i < leng:
+            result_str = []
+            while j < leng:
+                result_str.append(max([data[i][j], data[i][j + 1], data[i + 1][j], data[i + 1][j + 1]]))
+                j += 2
+            i += 2
+            j = 0
+            result.append(result_str)
+        return np.array(result)
 
-    def train(self, X_train, y_train, learning_rate=0.01, epochs=10):
-        for epoch in range(epochs):
-            for index, image in enumerate(X_train):
-                # Расширение размерности для корректного проведения операции свертки
-                image = image.reshape((1, 28, 28, 1))
-                conv1_output = self.convolve2d(image, self.filters1)
+    def fully_connected(self, flattened_output):
+        return np.dot(self.flatten_weight, flattened_output)
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def train(self, X, y, epoch, learning_rate=0.01):
+        for eph in range(epoch):
+            for index_training_example, training_example in enumerate(X):
+                training_example = np.array(training_example)
+                conv1_output = self.convolve2d(training_example)
                 pool1_output = self.max_pooling(conv1_output)
 
-                # Reshape the pool1_output tensor to have a shape of (height, width, 1, num_filters)
-                pool1_output = pool1_output.reshape(
-                    (pool1_output.shape[0], pool1_output.shape[1], 1, pool1_output.shape[2]))
+                conv2_output = self.convolve2d(pool1_output)
+                pool21_output = self.max_pooling(conv2_output)
 
-                conv2_output = self.convolve2d(pool1_output, self.filters2)
-                pool2_output = self.max_pooling(conv2_output)
+                flattened_output = self.flatten(pool21_output)
+                fully_connected = self.fully_connected(flattened_output)
+                # print('fully_connected', fully_connected)
+                output = self.sigmoid(fully_connected)
+                # print('output', output)
+                # print('y[index_training_example]', y[index_training_example])
+                # Backward pass
+                loss_gradient = output - y[index_training_example]
+                fc_output_gradient = np.dot(self.flatten_weight.T, loss_gradient)
+                fc_output_gradient[fc_output_gradient < 0] = 0
 
+                # Update weights
+                self.flatten_weight -= learning_rate * np.outer(loss_gradient, flattened_output)
 
-                flattened_output = self.flatten(pool2_output)
+                # Optionally, update convolutional layer weights here
 
-                print("pool1_output shape:", pool1_output.shape)
-                print("pool2_output shape:", pool2_output.shape)
-                print("flattened_output shape:", flattened_output.shape)
-                # print("fc_output shape:", fc_output.shape)
-
-                fc_output = self.fully_connected(flattened_output, self.weights_fc)
-                fc_output_relu = np.maximum(0, fc_output)
-
-
-                output = self.fully_connected(fc_output_relu, self.weights_output)
-                predicted_probs = self.softmax(output)
-
-                loss_gradient = predicted_probs - y_train[index]
-                weights_output_gradient = np.outer(fc_output_relu, loss_gradient)
-                fc_output_gradient = np.dot(self.weights_output, loss_gradient)
-                fc_output_gradient[fc_output < 0] = 0
-                weights_fc_gradient = np.outer(flattened_output, fc_output_gradient)
-
-                self.weights_output -= learning_rate * weights_output_gradient
-                self.weights_fc -= learning_rate * weights_fc_gradient.reshape(self.weights_fc.shape)
-
-    def predict(self, X_test):
-        predictions = []
-        for image in X_test:
-            # Расширение размерности для корректного проведения операции свертки
-            image = image.reshape((1, 28, 28, 1))
-            conv1_output = self.convolve2d(image, self.filters1)
-            pool1_output = self.max_pooling(conv1_output)
-
-            conv2_output = self.convolve2d(pool1_output, self.filters2)
-            pool2_output = self.max_pooling(conv2_output)
-
-            flattened_output = self.flatten(pool2_output)
-            fc_output = self.fully_connected(flattened_output, self.weights_fc)
-            fc_output_relu = np.maximum(0, fc_output)
-
-            output = self.fully_connected(fc_output_relu, self.weights_output)
-            predictions.append(output)
-
-        return predictions
+                # Print or log the loss
+                if index_training_example % 1000 == 0:
+                    current_loss = np.mean(-y_train * np.log(output) - (1 - y_train) * np.log(1 - output))
+                    print(f'Epoch {eph}, Example {index_training_example}, Loss: {current_loss}')
 
 
 if __name__ == '__main__':
     X_train, y_train = get_train_data()
-    num_classes = 10
-    print(X_train.shape)
-    # simple_conv_net = CNN(input_size=X_train.shape, num_classes)
-    # simple_conv_net.train(X_train, y_train)
-    # predictions = simple_conv_net.predict(X_test)
+    simple_conv_net = CNN(
+        input_size=X_train[0].shape[0],
+        num_classes=len(np.unique(y_train)),
+        count_convolutional_layer=1)
+    simple_conv_net.train(X=X_train, y=y_train, epoch=1)
