541daff7f4dab501fd542e578f3adc7531c563b8
filip <102216221+FilippYV@users.noreply.github.com>
2023-11-16 04:38:43 +0300

    gnn test v2

--- 10. Graph_neural_network/Graph_neural_network.py
+++ 10. Graph_neural_network/Graph_neural_network.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, classification_report, confusion_matrix
 import plotly.express as px
-import matplotlib.pyplot as plt
-import random
+
 
 
 class GraphNeuralNetwork:
@@ -12,11 +11,9 @@ class GraphNeuralNetwork:
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
-        # Веса для скрытого слоя и выходного слоя
         self.weights_hidden = np.random.rand(self.input_dim, self.hidden_dim)
         self.weights_output = np.random.rand(self.hidden_dim, self.output_dim)
 
-        # Смещения для скрытого слоя и выходного слоя
         self.bias_hidden = np.random.rand(self.hidden_dim)
         self.bias_output = np.random.rand(self.output_dim)
 
@@ -27,7 +24,6 @@ class GraphNeuralNetwork:
         return x * (1 - x)
 
     def forward_pass(self, inputs):
-        # Прямой проход через сеть
         hidden = self.sigmoid(np.dot(inputs, self.weights_hidden) + self.bias_hidden)
         output = self.sigmoid(np.dot(hidden, self.weights_output) + self.bias_output)
         return output
@@ -35,19 +31,15 @@ class GraphNeuralNetwork:
     def train(self, inputs, labels, epochs, learning_rate):
         for epoch in range(epochs):
             for i in range(len(inputs)):
-                # Прямой проход
                 hidden = self.sigmoid(np.dot(inputs[i], self.weights_hidden) + self.bias_hidden)
                 output = self.sigmoid(np.dot(hidden, self.weights_output) + self.bias_output)
 
-                # Ошибка на выходном слое
                 output_error = labels[i] - output
                 output_delta = output_error * self.sigmoid_derivative(output)
 
-                # Ошибка на скрытом слое
                 hidden_error = np.dot(output_delta, self.weights_output.T)
                 hidden_delta = hidden_error * self.sigmoid_derivative(hidden)
 
-                # Обновление весов и смещений
                 self.weights_output += learning_rate * np.dot(hidden.reshape(-1, 1), output_delta.reshape(1, -1))
                 self.bias_output += learning_rate * output_delta
                 self.weights_hidden += learning_rate * np.dot(inputs[i].reshape(-1, 1), hidden_delta.reshape(1, -1))
@@ -58,46 +50,48 @@ class GraphNeuralNetwork:
         for i in range(len(inputs)):
             prediction = self.forward_pass(inputs[i])
             predictions.append(prediction)
+        predictions = [int(item.flatten()[0] > 0.5) for item in predictions]
         return predictions
 
-def metrics_printing(y_true, y_pred):
+
+def metrics_printing(y_true, y_pred, filename):
+    print(f'Для {filename}')
     print(f'Mean Squared Error: {mean_squared_error(y_true, y_pred)}')
     print(f'R-squared: {r2_score(y_true, y_pred)}')
     print(f'Mean Absolute Error: {mean_absolute_error(y_true, y_pred)}')
     print(classification_report(y_true, y_pred))
 
+
 def out_confusion_matrix(y_true, y_pred):
     fig = px.imshow(confusion_matrix(labels, y_pred), text_auto=True)
     fig.update_layout(xaxis_title='Цель', yaxis_title='Прогноз')
     fig.show()
+
+
 def get_data_from_csv(filename):
-    # Считываем данные из CSV файла
-    data = pd.read_csv(f'{filename}')
-
-    # Получаем признаки (исключая столбец с метками)
+    data = pd.read_csv(f'../Datasets/for_gnn/{filename}')
     features = data.drop(['label'], axis=1).values
-
-    # Нормализуем данные (масштабирование)
     features = (features - features.mean(axis=0)) / features.std(axis=0)
-
-    # Получаем метки
     labels = data['label'].values
     return features, labels
 
 
 if __name__ == '__main__':
-    features, labels = get_data_from_csv(filename='graph_data.csv')
+    filename = 'graph_data.csv'
+    features, labels = get_data_from_csv(filename=filename)
     input_dim = features.shape[1]
-    hidden_dim = 8  # Размер скрытого слоя (можно изменить)
-    output_dim = 1  # Выходной слой для бинарной классификации
+    hidden_dim = 15
+    output_dim = 1
 
-    # Инициализируем и обучаем нейронную сеть
-    nn = GraphNeuralNetwork(input_dim, hidden_dim, output_dim)
-    nn.train(features, labels, epochs=1000, learning_rate=0.1)
+    gnn = GraphNeuralNetwork(input_dim, hidden_dim, output_dim)
+    gnn.train(features, labels, epochs=2000, learning_rate=0.1)
 
-    y_pred = nn.predict(features)
-    y_pred = [int(item.flatten()[0] > 0.5) for item in y_pred]
-    metrics_printing(labels, y_pred)
+    y_pred = gnn.predict(features)
+    metrics_printing(labels, y_pred, filename)
     out_confusion_matrix(labels, y_pred)
 
-    features, labels = get_data_from_csv(filename='graph_data_test.csv')
+    filename = 'graph_data_test.csv'
+    features, labels = get_data_from_csv(filename=filename)
+    y_pred = gnn.predict(features)
+    metrics_printing(labels, y_pred, filename)
+    out_confusion_matrix(labels, y_pred)
